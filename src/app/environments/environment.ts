943139661b18574bccd0101efb4b56ca52e14edb
3kdono <hplegislador@yahoo.com>
2018-01-22 14:59:18 +0800

    initial commit

---
+++ src/app/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAkAIrGsRRt-j5drx4eV7OHLVZ7Hi6xJ-U",
+    authDomain: "firestarter-3b2a6.firebaseapp.com",
+    databaseURL: "https://firestarter-3b2a6.firebaseio.com",
+    projectId: "firestarter-3b2a6",
+    storageBucket: "firestarter-3b2a6.appspot.com",
+    messagingSenderId: "432396742479"
+
+    }
+};
